3d6f64eb0d8088b772a9bfed84c31ab3246f5b12
iflix0fox <ahmadalazzawi5080@gmail.com>
2025-10-30 12:06:27 +0200

    arranging files

---
+++ hr-app/src /App.jsx
@@ -0,0 +1,49 @@
+import "./App.css";
+import Card from "./components/Card";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <main className="main-content">
+        <Card
+          name="Maria"
+          title="CEO"
+          salary="5000€"
+          phone="040 123 4567"
+          email="maria@example.com"
+          animal="Cat"
+        />
+        <Card
+          name="Kati"
+          title="Developer"
+          salary="4200€"
+          phone="040 765 4321"
+          email="kati@example.com"
+          animal="Dog"
+        />
+        <Card
+          name="Karin"
+          title="Designer"
+          salary="3900€"
+          phone="050 987 6543"
+          email="karin@example.com"
+          animal="Rabbit"
+        />
+        <Card
+          name="Brad"
+          title="Designe manger"
+          salary="7900€"
+          phone="050 111 6543"
+          email="brad@example.com"
+          animal="cat"
+        />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ hr-app/src /components/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>© 2025 WP25K</p>
+    </footer>
+  );
+};
+
+export default Footer;
